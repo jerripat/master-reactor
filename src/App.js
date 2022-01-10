a65411814882fe3427e220e13576aaac2d0bf4eb
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-10 09:21:45 -0600

    singlereactfile

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div>
-          <h2 title= "This is the Heading"> Heading - This is JSX method</h2>
-          <p id="para1">This is a paragraph</p>
-          <p className = "para2">This is a second paragraph</p>
-            <div>
-                <label htmlFor="">Some Data</label>
-                <input id="input1" name="input1"></input>
-             </div>
-      
-      
-      
+      <h2 title="This is the Heading"> Heading - This is JSX method</h2>
+      <p id="para1">This is a paragraph</p>
+      <p className="para2">This is a second paragraph</p>
+      <div>
+        <label htmlFor="input1"> Some Data</label>
+        <input id="input1" name="input1"></input>
+      </div>
     </div>
-  )
-  
+  );
 }
 
 export default App;

1869b7b054e24e1b023ffdc083b4fe92619795c1
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-09 20:55:02 -0600

    JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+          <h2 title= "This is the Heading"> Heading - This is JSX method</h2>
+          <p id="para1">This is a paragraph</p>
+          <p className = "para2">This is a second paragraph</p>
+            <div>
+                <label htmlFor="">Some Data</label>
+                <input id="input1" name="input1"></input>
+             </div>
+      
+      
+      
     </div>
-  );
+  )
+  
 }
 
 export default App;
